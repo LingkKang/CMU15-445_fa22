38a1555034879d70aa13a9060513ca09100928f9
Lingkang <karlzhu12@gmail.com>
2022-10-04 14:16:01 +0800

    hw1_SQL q5

---
+++ hw1_SQL/q5_decade_ratings.sql
@@ -0,0 +1,22 @@
+SELECT
+    decades,
+    ROUND(AVG(rating), 2) AS avg_rating,
+    MAX(rating),
+    MIN(rating),
+    COUNT(decades)
+
+    FROM ratings
+    JOIN (
+        SELECT
+            title_id,
+            CAST(CAST(ROUND(premiered / 10) * 10 AS INT) AS TEXT) || 's' AS decades
+
+            FROM titles
+            WHERE premiered IS NOT NULL
+    ) AS title_with_decades
+
+    ON title_with_decades.title_id = ratings.title_id
+    GROUP BY decades
+    ORDER BY
+        avg_rating DESC,
+        decades ASC;
