12d3bde85a6f96966de11a6d40b60c2ffb71497e
Lingkang <karlzhu12@gmail.com>
2022-10-04 15:02:20 +0800

    Update coding style

--- hw1_SQL/q2_sci_fi.sql
+++ hw1_SQL/q2_sci_fi.sql
@@ -1,5 +1,8 @@
-SELECT primary_title, premiered, runtime_minutes || ' (mins)'
-    FROM titles
-    WHERE genres LIKE '%Sci-Fi%'
-    ORDER BY runtime_minutes DESC
-    LIMIT 10;
+SELECT 
+    primary_title, 
+    premiered, 
+    runtime_minutes || ' (mins)' 
+FROM titles 
+WHERE genres LIKE '%Sci-Fi%' 
+ORDER BY runtime_minutes DESC 
+LIMIT 10;
--- hw1_SQL/q5_decade_ratings.sql
+++ hw1_SQL/q5_decade_ratings.sql
@@ -1,22 +1,19 @@
-SELECT
-    decades,
-    ROUND(AVG(rating), 2) AS avg_rating,
-    MAX(rating),
-    MIN(rating),
-    COUNT(decades)
-
-    FROM ratings
-    JOIN (
-        SELECT
-            title_id,
-            CAST(CAST(ROUND(premiered / 10) * 10 AS INT) AS TEXT) || 's' AS decades
-
-            FROM titles
-            WHERE premiered IS NOT NULL
-    ) AS title_with_decades
-
-    ON title_with_decades.title_id = ratings.title_id
-    GROUP BY decades
-    ORDER BY
-        avg_rating DESC,
-        decades ASC;
+SELECT 
+    decades, 
+    ROUND(AVG(rating), 2) AS avg_rating, 
+    MAX(rating), 
+    MIN(rating), 
+    COUNT(decades) 
+FROM ratings 
+JOIN (
+    SELECT 
+        title_id, 
+        CAST(CAST(ROUND(premiered / 10) * 10 AS INT) AS TEXT) || 's' AS decades 
+    FROM titles 
+    WHERE premiered IS NOT NULL 
+) AS title_with_decades 
+ON title_with_decades.title_id = ratings.title_id 
+GROUP BY decades 
+ORDER BY 
+    avg_rating DESC, 
+    decades ASC;
