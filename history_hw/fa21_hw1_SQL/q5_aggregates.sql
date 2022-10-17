5e3287ac1f6a2e1d9c2dc09c6cf283ba0502d6e9
Lingkang <karlzhu12@gmail.com>
2022-10-17 14:33:37 +0800

    fa21_hw1_sql check ans q5

--- history_hw/fa21_hw1_SQL/q5_aggregates.sql
+++ history_hw/fa21_hw1_SQL/q5_aggregates.sql
@@ -3,9 +3,14 @@ SELECT
     COUNT(CategoryId) AS cat_count,
     ROUND(AVG(UnitPrice), 2) AS avg_price,
     MIN(UnitPrice) AS min_price,
-    MAX(UnitPrice) AS max_price
+    MAX(UnitPrice) AS max_price,
+    SUM(UnitsOnOrder) AS total
 FROM Product
 JOIN
     Category ON Category.Id = CategoryId
 GROUP BY
+    CategoryId
+HAVING
+    cat_count > 10
+ORDER BY
     CategoryId;
